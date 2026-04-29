13e3151de2f2bd64b541e4fe0c731c64f2785cc6
sohel160 <sohelcmc53rd@gmail.com>
2026-04-29 10:04:25 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -35,47 +35,31 @@ export default {
     const proxies = `
 proxies:
 
-  - name: speed1
-    type: socks5
-    server: 180.92.235.222
-    port: 16626
-    username: ub3977bb7d309
-    password: 1-2LYkl4rMC3D1xy
-
-  - name: speed2
-    type: socks5
-    server: 180.92.229.54
-    port: 16626
-    username: uf7c149982eeb
-    password: 3j8M5GOJBxbFphzj
-
-  - name: speed3
-    type: socks5
-    server: 103.79.178.206
-    port: 9168
-    username: ua20c6877f504
-    password: lnrkpbqS80fLBwlK
-
-  - name: speed4
-    type: socks5
-    server: 103.239.252.42
-    port: 1088
-    username: u24e3a422c037
-    password: HKEFMKYOPm1bn3ph
-
-  - name: speed5
-    type: socks5
-    server: 182.252.77.160
-    port: 9168
-    username: u541179755e11
-    password: bjxGnakmxN-jTxGg
-
-  - name: speed6
-    type: socks5
-    server: 103.48.161.248
-    port: 1080
-    username: u57787b7a23b0
-    password: IqXi9LCfTfX1Z8hV
+- name: proxy1
+  type: http
+  server: 144.48.108.121
+  port: 5452
+- name: proxy2
+  type: http
+  server: 103.69.150.114
+  port: 9855
+- name: proxy3
+  type: http
+  server: 103.109.96.20
+  port: 9862
+- name: proxy4
+  type: http
+  server: 103.69.150.138
+  port: 9859
+- name: proxy5
+  type: http
+  server: 27.147.195.166
+  port: 27271
+- name: proxy6
+  type: http
+  server: 103.198.132.93
+  port: 2610
+  
 `
 
     return new Response(proxies, {
